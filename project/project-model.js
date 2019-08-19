6faf38ff838b90164ced5a497ef26aa9d9e07fb9
Ihab Ali <ihab@live.com>
2019-08-19 01:27:19 -0500

    added project-model.js helper methods

--- project/project-model.js
+++ project/project-model.js
@@ -2,13 +2,38 @@ const db = require("../data/dbConfig");
 
 module.exports = {
   find,
-  add
+  add,
+  findById,
+  addTask,
+  findAllTasks
 };
 
 function find() {
   return db("projects");
 }
+function findById(id) {
+  return db("projects").where({ id });
+}
 
-function add(info) {
-  return db("projects").insert(info);
+async function add(info) {
+  const [id] = await db("projects").insert(info);
+  return db("projects").where({ id });
+}
+
+function findByIdTask(id) {
+  return db("tasks")
+    .where({ id })
+    .first();
+}
+
+async function addTask(info, project_id) {
+  console.log(info);
+  const [id] = await db("tasks").insert({ ...info, project_id });
+  return findByIdTask(id);
+}
+
+function findAllTasks(id) {
+  return db("tasks as t")
+    .join("projects as p", "project_id", "p.id")
+    .select("t.*");
 }
